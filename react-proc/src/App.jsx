6753e08ec287dc0aae48a6c26c3e84060279c0a0
Kane Dylan <cool.kdtech1234@gmail.com>
2025-01-18 20:27:14 +0530

    todays update

--- react-proc/src/App.jsx
+++ react-proc/src/App.jsx
@@ -1,11 +1,13 @@
 import Footer from "./components/Footer"
 import Header from "./components/Header"
 import MainContent from "./components/MainContent"
+import WelcomeMessage from "./components/WelcomeMessage"
 
 const App  = () => {
   return  (
     <div>
       <Header/>
+      <WelcomeMessage/>
       <MainContent/>
       <Footer/>
     </div>
---
+++ react-proc/src/components/WelcomeMessage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const WelcomeMessage = () => {
+  return (
+    <div>
+      <h>Hello, World!
+        <p>Welcome to learning JSX!</p>
+      </h>
+    </div>
+  )
+}
+
+export default WelcomeMessage
