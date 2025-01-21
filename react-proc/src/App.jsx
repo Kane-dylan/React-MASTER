c71a672997ee0443a97ffde8f32f86e7e84af290
Kane Dylan <cool.kdtech1234@gmail.com>
2025-01-21 13:09:31 +0530

    todays update-21/01/25

--- react-proc/src/App.jsx
+++ react-proc/src/App.jsx
@@ -2,10 +2,12 @@ import Footer from "./components/Footer"
 import Header from "./components/Header"
 import MainContent from "./components/MainContent"
 import WelcomeMessage from "./components/WelcomeMessage"
+import JSXRules from "./JSXRules"
 
 const App  = () => {
   return  (
     <div>
+      <JSXRules/>
       <Header/>
       <WelcomeMessage/>
       <MainContent/>
