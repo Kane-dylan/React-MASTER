e8e69533f529676ae82843a8c1fcd0f143e505c9
Kane Dylan <cool.kdtech1234@gmail.com>
2025-02-13 01:40:02 +0530

    changes

--- react-proc/src/components/Greeting.jsx
+++ react-proc/src/components/Greeting.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
+import { FaSun } from "react-icons/fa";
+import { FaMoon } from "react-icons/fa";
+import React from "react";
 
-const Greeting = ({timeOfDay}) => {
-  if(timeOfDay == "morning"){
-    return<h1>Good Morning</h1>
-  } else if(timeOfDay == "afternoon"){
-    return<h1>Good Afternoon</h1>
-  } else if (timeOfDay == "evening"){
-    return<h1>Good Evening</h1>
+const Greeting = ({ timeOfDay }) => {
+  if (timeOfDay == "morning") {
+    return <h1>Good Morning
+      <FaSun/>
+    </h1>;
+  } else if (timeOfDay == "afternoon") {
+    return <h1>Good Afternoon</h1>;
+  } else if (timeOfDay == "evening") {
+    return <h1>Good Evening
+      <FaMoon/>
+    </h1>;
   }
-}
+};
 
-export default Greeting
+export default Greeting;
