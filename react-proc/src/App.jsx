0925d6cc2488fab110ee421213118c6375dd3342
Kane Dylan <cool.kdtech1234@gmail.com>
2025-02-14 15:31:12 +0530

    changes

--- react-proc/src/App.jsx
+++ react-proc/src/App.jsx
@@ -1,18 +1,25 @@
 import React, { useState } from 'react'
 
 const App = () => {
-  const [count, setCount] = useState(0)
-    
 
-return (
+  const [friends, setFriends] = useState(["john" , "peter" , "susan"])
+  const addNewFriend = () => setFriends([...friends, "jane"])
+  const removeFriend = () => setFriends(friends.filter((f) => f !== "jane"))
+
+  const updateFriend = () => {
+    setFriends(friends.map((f)=> (f === "jane" ? "jane doe" : f)))
+  }
   
-<div>
-  <h1>Counter</h1>
-  <p>{count}</p>
-  <button onClick={() => setCount(count + 1)}>+</button>
-  <button onClick={() => setCount(count - 1)}>-</button>
-</div>
-)
+  return (
+    <div>
+      {friends.map((f)=>(
+    <li key={Math.random()}>{f}</li>
+    ))}
+      <button onClick={addNewFriend}>Add another friend </button>
+      <button onClick={removeFriend}>Remove one friends</button>
+      <button onClick={updateFriend}>Update one friend</button>
+    </div>
+  )
 }
 
 export default App
