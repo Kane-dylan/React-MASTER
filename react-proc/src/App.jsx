47c3ae4ae501f1032012ea5707c415402f126219
Kane Dylan <cool.kdtech1234@gmail.com>
2025-02-15 03:03:10 +0530

    changes

--- react-proc/src/App.jsx
+++ react-proc/src/App.jsx
@@ -3,37 +3,34 @@ import React, { useState } from 'react'
 
 const App = () => {
 
-  const [movies, setMovies] = useState(
-    {
+  const [movies, setMovies] = useState([
+    { id: 1,
       title: "The Godfather",
       ratings: 7.2
-    }
-    // {
-    //   title: "The Shawshank Redemption",      
-    //   ratings: 8.6
-    // },
-    // {
-    //   title: "Forrest Gump",
-    //   ratings: 4.3
-    // },
-  );
+    },
+    { id: 2, 
+      title: "The Shawshank Redemption",      
+      ratings: 8.6
+    },
+    { id: 3,
+      title: "Forrest Gump",
+      ratings: 4.3
+    },
+  ]);
 
-  const updateRatings = () => {
-    const updatedMovies = {
-      ...movies,
-      ratings: 8.2 
-    }
-
-    setMovies(updatedMovies);
+  const updateName = () => {
+    setMovies(
+      movies.map((m)=>(m.id === 2 ? {...movies, title: "John Wick 4"} : m))
+    )
   }
+  
 
   return (
     <div>
-      <h1>My Favorite Movies</h1>
-      <li>{movies.title}</li>
-      <li>{movies.ratings}</li>
-
-      <button onClick={updateRatings}>Update Movie Ratings</button>
+      {movies.map((m) => (
+        <li key={Math.random()}>{m.title}</li>
+      ))}
+      <button onClick={updateName}>Change Name</button>
     </div>
   );
 }
